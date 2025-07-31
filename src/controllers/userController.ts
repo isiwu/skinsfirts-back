46156f9ae980e75ffb42292ce35260eb08a5c516
emmanuel <isiwuemmma@gmail.com>
2025-07-31 22:46:06 +0100

    prisma orm integration

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,13 @@
+import { PrismaClient } from "@prisma/client";
 import { Request, Response } from "express";
 
 export default class UserController {
+  private prisma;
+
+  constructor() {
+    this.prisma = new PrismaClient
+  }
+  
   user = (req: Request, res: Response) => {
     res.json({
       status: true,
