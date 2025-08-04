c2a500afd32fe3abcc08f11207689989d4792f10
emmanuel <isiwuemmma@gmail.com>
2025-08-04 09:32:12 +0100

    doctor update and available controller

--- src/controllers/doctorController.ts
+++ src/controllers/doctorController.ts
@@ -40,7 +40,38 @@ export default class DoctorController {
 
     res.json({status: true, data: doctor})
   }
-  doctors = async (re: Request, res: Response) => {
+  updateDoctor = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const file = req.file;
+    const { name, field, focus, profile, careerPath, highlight, gender, experience } = req.body;
+
+    // var doctor: any;
+    var fileUrl = `${req.protocol}://${req.headers.host}/images/${file?.filename}`
+    const newGender: Gender = gender==="male"? "male":"female";
+
+    try {
+      const doc = await this.prisma.doctor.findUnique({ where: { id }});
+      const doctor = await this.prisma.doctor.update({
+        where: { id },
+        data: {
+          name: name ?? doc?.name,
+          field: field ?? doc?.field,
+          image: fileUrl ?? doc?.image,
+          focus: focus ?? doc?.focus,
+          profile: profile ?? doc?.profile,
+          careerPath: careerPath ?? doc?.careerPath,
+          highlight: highlight ?? doc?.highlight,
+          gender: newGender,
+          experience: experience ?? doc?.experience
+        }
+      });
+
+      res.json({status: true, data: doctor})
+    } catch (error) {
+      return res.json({status: false, data: error});
+    }
+  }
+  doctors = async (req: Request, res: Response) => {
     var doctors: any;
     try {
       doctors = await this.prisma.doctor.findMany({});
@@ -62,24 +93,37 @@ export default class DoctorController {
 
     res.json({status: true, data: doctor})
   }
-  updateDoctor = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  availability = async (req: Request, res: Response) => {
+    const { date, times, doctorId } = req.body;
 
-    var doctor: any;
-    // try {
-    //   doctor = await this.prisma.doctor.update({
-    //     where: { id },
-    //     data: {
-    //       datesNotAvailable: {
-    //         push: new Date(),
-    //       },
-    //       timesNotAvailable: {
-    //         push: new Date()
-    //       }
-    //     }
-    //   })
-    // } catch (error) {
-    //   return res.json({status: false, data: error});
-    // }
+    var availability1: any;
+    var availableTimes1: any;
+
+    try {
+      const availability = await this.prisma.availability.create({
+        data: {
+          doctorId,
+          date: new Date(date),
+        }
+      })
+
+      for (let i = 0; i < times.length; i++) {
+        for (let j = 0; j < times[i].length; j++) {
+          await this.prisma.time.create({
+            data: {
+              availableId: availability.id,
+              startTime: times[i][j][0],
+              endTime: times[i][j][1]
+            }
+          });
+        }
+      }
+
+      const available = await this.prisma.availability.findFirst({ where: { id: availability.id }});
+
+      res.json({status: true, data: available})
+    } catch (error) {
+      return res.json({status: false, data: error});
+    }
   }
 }
