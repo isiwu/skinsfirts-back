a01194bc5dead6f576b2ff57dc6578d9286fbd09
emmanuel <isiwuemmma@gmail.com>
2025-08-05 16:17:16 +0100

    patient index route added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userRouter from './users';
 import doctorRouter from "./doctor";
+import patientRouter from "./patient";
 
 const router = Router();
 
 router.use('/users', userRouter);
 router.use("/doctors", doctorRouter);
+router.use("/patients", patientRouter);
 
 export default router;
